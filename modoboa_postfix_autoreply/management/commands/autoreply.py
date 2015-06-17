cb32a77a0c400fb0adaaa6473f59afc6928158c6
Stéphane Klein <contact@stephane-klein.info>
2015-06-17 23:43:31 +0200

    Append log message to help to understand autoreply mechanism

--- modoboa_postfix_autoreply/management/commands/autoreply.py
+++ modoboa_postfix_autoreply/management/commands/autoreply.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 import sys
 import datetime
+import logging
+from logging.handlers import SysLogHandler
+from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
@@ -15,10 +18,15 @@ from modoboa_admin.models import Mailbox
 from ...models import ARmessage, ARhistoric
 from ...modo_extension import PostfixAutoreply
 
+logger = logging.getLogger()
+logger.addHandler(SysLogHandler(address='/dev/log'))
+logger.setLevel(logging.ERROR)
+
 
 def send_autoreply(sender, mailbox, armessage):
     if armessage.fromdate > timezone.now():
         return
+
     if armessage.untildate is not None \
             and armessage.untildate < timezone.now():
         armessage.enabled = False
@@ -33,12 +41,21 @@ def send_autoreply(sender, mailbox, armessage):
         now = timezone.make_aware(datetime.datetime.now(),
                                   timezone.get_default_timezone())
         if lastar.last_sent + delta > now:
+            logger.debug(
+                'autoreply no mail send because delta (%s) < timetout (%s)' % (
+                    delta,
+                    timeout
+                )
+            )
             sys.exit(0)
+
     except ARhistoric.DoesNotExist:
         lastar = ARhistoric()
         lastar.armessage = armessage
         lastar.sender = sender
 
+    logger.debug(
+        'autoreply message send to %s' % mailbox.user.encoded_address)
     sendmail_simple(mailbox.user.encoded_address, sender, armessage.subject,
                     armessage.content.encode('utf-8'))
 
@@ -50,23 +67,42 @@ class Command(BaseCommand, CloseConnectionMixin):
     args = '<sender> <recipient ...>'
     help = 'Send autoreply emails'
 
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--debug', '-d', action='store_true', dest='debug', default=False
+        ),
+    )
+
     def handle(self, *args, **options):
+        if options['debug']:
+            logger.setLevel(logging.DEBUG)
+
         if len(args) < 2:
+            logger.debug("autoreply %s" % " ".join(args))
+
             raise CommandError(
                 "usage: ./manage.py autoreply <sender> <recipient ...>")
+
+        logger.debug(
+            "autoreply sender=%s recipient=%s" % (args[0], ",".join(args[1:]))
+        )
 
         PostfixAutoreply().load()
         sender = args[0]
         for fulladdress in args[1:]:
             address, domain = split_mailbox(fulladdress)
             try:
-                mbox = Mailbox.objects.get(address=address, domain__name=domain)
+                mbox = Mailbox.objects.get(
+                    address=address, domain__name=domain)
             except Mailbox.DoesNotExist:
-                print "Unknown recipient %s" % (fulladdress)
+                msg = "Unknown recipient %s" % (fulladdress)
+                logger.debug("autoreply %s" % msg)
                 continue
             try:
                 armessage = ARmessage.objects.get(mbox=mbox.id, enabled=True)
+                logger.debug("autoreply message found")
             except ARmessage.DoesNotExist:
+                logger.debug("autoreply message not found")
                 continue
 
             send_autoreply(sender, mbox, armessage)
